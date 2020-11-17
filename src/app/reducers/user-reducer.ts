d3bc65adf70315c556f7a9f39a68473f2cc90acc
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-11-17 17:53:04 +0530

    entities format

--- src/app/reducers/user-reducer.ts
+++ src/app/reducers/user-reducer.ts
@@ -1,19 +1,23 @@
 import {User} from '../models/user';
 import {Action} from '../actions';
 import {USER_ADD, USER_DELETE, USER_LIST_ERROR, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_UPDATE} from '../actions/user-action';
+import {StoreUtility} from '../utils/store-utility';
+import {createSelector} from '@ngrx/store';
 
 export interface UserReducerState {
   loading: boolean;
   loaded: boolean;
   error: boolean;
-  users: User[];
+  entities: { [id: number]: User };
+  ids: number[];
 }
 
 const initialState: UserReducerState = {
   loaded: false,
   loading: false,
   error: false,
-  users: []
+  entities: {},
+  ids: []
 };
 
 export function UserReducer(state = initialState, action: Action): UserReducerState {
@@ -22,24 +26,41 @@ export function UserReducer(state = initialState, action: Action): UserReducerSt
       return {...state, loading: true};
     }
     case USER_DELETE: {
-      const users = state.users.filter(data => data.id !== action.payload.id);
-      return {...state, ...{users}};
+      const id = action.payload.id;
+      const newIds = state.ids.filter(elem => elem !== id);
+      const newEntities = StoreUtility.removeKey(state.entities, id);
+      return {...state, ...{entities: newEntities, ids: newIds}};
     }
     case USER_UPDATE: {
-      const users = state.users.filter(data => data.id !== action.payload.data.id);
-      const updatedUser = users.concat(action.payload.data);
-      return {...state, ...{users: updatedUser}};
+      const user = action.payload.data;
+      const entity = {[user.id]: user};
+      const updatedEntities = {...state.entities, ...entity};
+      return {...state, ...{entities: updatedEntities}};
     }
     case USER_ADD: {
-      const users = state.users.concat(action.payload.data);
-      return {...state, ...{users}};
+      const user = action.payload.data;
+      const entity = {[user.id]: user};
+      const newEntities = {...state.entities, ...entity};
+      const newIds = StoreUtility.filterDuplicateIds([...state.ids, user.id]);
+      return {...state, ...{entities: newEntities, ids: newIds}};
+
     }
     case USER_LIST_ERROR: {
       return {...state, error: true, loading: false};
     }
     case USER_LIST_SUCCESS: {
-      const updatedUsers = state.users.concat(action.payload.data);
-      return {...state, loading: false, loaded: true, users: updatedUsers, error: false};
+      const users = action.payload.data;
+      const obj = StoreUtility.normalize(users);
+      const newEntities = {...state.entities, ...obj};
+      const ids = users.map(user => user.id);
+      const newIds = StoreUtility.filterDuplicateIds([...state.ids, ...ids]);
+      return {
+        ...state, ...{
+          loaded: true,
+          loading: false, error: false,
+          entities: newEntities, ids: newIds
+        }
+      };
     }
     default: {
       return state;
@@ -50,6 +71,9 @@ export function UserReducer(state = initialState, action: Action): UserReducerSt
 // selectors
 export const getLoading = (state: UserReducerState) => state.loading;
 export const getLoaded = (state: UserReducerState) => state.loaded;
-export const getUsers = (state: UserReducerState) => state.users;
+export const getEntities = (state: UserReducerState) => state.entities;
+export const getIds = (state: UserReducerState) => state.ids;
+export const getUsers = createSelector(getEntities,
+  (entities) => StoreUtility.unNormalized(entities));
 export const getError = (state: UserReducerState) => state.error;
 
